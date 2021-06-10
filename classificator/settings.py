006b418bdf8853cfb3517ba4acc5099ada8423be
ruselbyru <r.schetko@gmail.com>
2021-06-10 21:36:19 +0300

    redy to deploy

--- classificator/settings.py
+++ classificator/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,7 @@ STATICFILES_DIRS=[
     BASE_DIR.joinpath('static/'),
 ]
 
+STATIC_ROOT= BASE_DIR.joinpath('staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
